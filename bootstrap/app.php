d918227e85c5c424e4f9c0d543bd425bacb66307
zeroplan123 <ganifahri07@gmail.com>
2025-11-13 13:52:23 +0700

    fix login bug

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
             'is_admin' => \App\Http\Middleware\AdminMiddleware::class,
-            'is_user' => \App\Http\Middleware\IsUser::class,
+            'is_user' => \App\Http\Middleware\Isuser::class,
             'check.user.status' => \App\Http\Middleware\CheckUserStatus::class,
             'check.banned' => \App\Http\Middleware\CheckBannedUser::class,
         ]);
